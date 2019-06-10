ca96b04479be87b0326564de6b18a4a9ce19ad41
Deepak <deepak25mb@gmail.com>
2019-06-10 19:40:25 +0530

    Updates

---
+++ precache-manifest.ed364cc2f5f894a1cc9bac9d3f8010cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e59e53fe928b863942adfa5e63d936",
+    "url": "/8-redux_blog_posts/index.html"
+  },
+  {
+    "revision": "fbd6f82c7a563e6cf385",
+    "url": "/8-redux_blog_posts/static/css/main.0a5069a7.chunk.css"
+  },
+  {
+    "revision": "cd4a533945a4ff319e0f",
+    "url": "/8-redux_blog_posts/static/js/2.75b13bf4.chunk.js"
+  },
+  {
+    "revision": "fbd6f82c7a563e6cf385",
+    "url": "/8-redux_blog_posts/static/js/main.f001294f.chunk.js"
+  },
+  {
+    "revision": "c3578e76feb97c1ffae8",
+    "url": "/8-redux_blog_posts/static/js/runtime~main.22cb1c70.js"
+  }
+]);
